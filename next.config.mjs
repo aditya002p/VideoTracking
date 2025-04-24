ce88d023107c3bcd00ec31b51fb25ffc01e4ad62
Aditya <ricky.pathak902@gmail.com>
2025-04-24 16:39:30 +0530

    updated the app folder

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    webpack(config) {
+      return config;
+    },
+    async rewrites() {
+      return [
+        {
+          source: '/api/:path*',
+          destination: process.env.NEXT_PUBLIC_API_URL + '/:path*' // Proxy API requests
+        }
+      ];
+    }
+  };
 
 export default nextConfig;
